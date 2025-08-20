05444279f6b7d9f7dcc02a226d3ebe2eb17cb309
op-qwerty4000 <chipscrispy05@gmail.com>
2025-08-20 23:23:50 +0400

    Add files via upload

---
+++ data-types.py
@@ -0,0 +1,21 @@
+#task 1
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+c = int(input("Enter the third number"))
+
+d = int(str(a) + str(b) + str(c))
+print(d)
+
+#task 2
+number = input("Enter a four-digit number: ")  # Get input as a string
+
+# Extract each digit and convert to integer
+digit1 = int(number[0])
+digit2 = int(number[1])
+digit3 = int(number[2])
+digit4 = int(number[3])
+#task 3
+
+ls_2 = float(input("Enter a number:"))
+
+print(ls_2)
